65acfc408f01e9bf8d02d80e8169ffb527646cee
woltes <30497385+woltes@users.noreply.github.com>
2017-12-08 11:27:20 +0300

    без Function.identity()

--- Stepic_6.4_13/src/Main.java
+++ Stepic_6.4_13/src/Main.java
@@ -8,16 +8,16 @@ public class Main {
 
 
     public static void main(String[] args) {
-        InputStream in = System.in;
+        /*InputStream in = System.in;
         ArrayList<String> list = new ArrayList<String>();
         try(Scanner s = new Scanner(in).useDelimiter(Pattern.compile("[\\p{Blank}\\p{Punct}]+"))) {
             while (s.hasNext()){
                 list.add(s.next().toLowerCase());
             }
 
-        }
+        }*/
         //System.out.println(list);
-
+        /*
         Map<String, Long> result =
                 list.stream().collect(
                         Collectors.groupingBy(
@@ -32,12 +32,24 @@ public class Main {
                 .sorted(Map.Entry.<String, Long>comparingByValue()
                         .reversed().thenComparing(e -> e.getKey())).limit(10).forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
 
-        //System.out.println(prefinalMap);
+
         List<String> al = new ArrayList<String>(finalMap.keySet());
         for (String str : al){
             System.out.println(str);
         }
-        //System.out.println(al);
+        */
 
+        Scanner s = new Scanner(System.in).useDelimiter("[\\p{Blank}\\p{Punct}]+");
+        Map<String, Integer> finalMap = new HashMap<>();
+        s.forEachRemaining(scan -> finalMap.merge(scan.toLowerCase(), 1, (i,n) ->  i + n));
+        //Meтод merge принимает ключ, значение и функцию которая объединяет заданное значение и уже существующее в отображении, если таковое имеется.
+        // Если в отображении под заданным ключем значения нет, то ложит туда указанное значение.
+        System.out.println(finalMap);
+        finalMap.entrySet().stream()
+                .sorted(Comparator.<Map.Entry<String, Integer>>comparingInt(Map.Entry::getValue)
+                        .reversed().thenComparing(Map.Entry::getKey))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .forEach(System.out::println);
     }
 }
